f7728a573a3a25c7ca0c77a870e9431b344a8abd
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2023-06-06 10:15:55 +0100

    Legacy Prison APIs return NA details, not a list, rename accordingly

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/service/NonAssociationsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/service/NonAssociationsService.kt
@@ -1,15 +1,12 @@
 package uk.gov.justice.digital.hmpps.hmppsnonassociationsapi.service
 
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.hmppsnonassociationsapi.dto.NonAssociationsDetails
+import uk.gov.justice.digital.hmpps.hmppsnonassociationsapi.dto.prisonapi.NonAssociationDetails
 
-// TODO: Check annotation, type in Incentives API was different
 @Service
 class NonAssociationsService {
 
-  fun getDetails(bookingId: Long): NonAssociationsDetails {
-    return NonAssociationsDetails(
-      offenderNo = "TODO: Implement me",
-    )
+  fun getDetails(bookingId: Long): NonAssociationDetails {
+    return prisonApiService.getNonAssociationDetails(bookingId)
   }
 }
