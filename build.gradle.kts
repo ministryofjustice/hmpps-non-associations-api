26efbc0708d274ae257271f40ea5f80385a0fc9e
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2023-06-08 17:35:26 +0100

    Removed unnecessary dependencies (as of now)
    and sorted test dependencies alphabetically to make it easier to compare
    with Incentives API.

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,16 +25,12 @@ dependencies {
 
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
 
-  implementation("org.apache.commons:commons-lang3")
-  implementation("org.apache.commons:commons-text:1.10.0")
-
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
+  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.11.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
-  testImplementation("javax.xml.bind:jaxb-api:2.3.1")
 }
 
 java {
