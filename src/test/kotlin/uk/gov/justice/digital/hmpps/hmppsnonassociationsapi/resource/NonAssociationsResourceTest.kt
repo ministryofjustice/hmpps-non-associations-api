d2622e916f6743518ffd627b4d7c574e6d3a3110
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2023-07-04 15:39:51 +0100

    NON-86: Stub of POST /non-associations tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/resource/NonAssociationsResourceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/resource/NonAssociationsResourceTest.kt
@@ -9,6 +9,110 @@ class NonAssociationsResourceTest : IntegrationTestBase() {
   final val prisonerNumber = "A1234BC"
 
   @Nested
+  inner class `Create a non-association` {
+
+    private val url = "/non-associations"
+
+    @Test
+    fun `without a valid token responds 401 Unauthorized`() {
+      webTestClient.post()
+        .uri(url)
+        .exchange()
+        .expectStatus()
+        .isUnauthorized
+    }
+
+    @Test
+    fun `without the correct role and scope responds 403 Forbidden`() {
+      // correct role, missing write scope
+      webTestClient.post()
+        .uri(url)
+        .headers(setAuthorisation(roles = listOf("ROLE_NON_ASSOCIATIONS")))
+        .header("Content-Type", "application/json")
+        .bodyValue(
+          // language=json
+          "{}",
+        )
+        .exchange()
+        .expectStatus()
+        .isForbidden
+
+      // correct role, missing write scope
+      webTestClient.post()
+        .uri(url)
+        .headers(
+          setAuthorisation(
+            roles = listOf("ROLE_NON_ASSOCIATIONS"),
+            scopes = listOf("read"),
+          ),
+        )
+        .header("Content-Type", "application/json")
+        .bodyValue(
+          // language=json
+          "{}",
+        )
+        .exchange()
+        .expectStatus()
+        .isForbidden
+    }
+
+    @Test
+    fun `without a valid request body responds 400 Bad Request`() {
+      // no request body
+      webTestClient.post()
+        .uri(url)
+        .headers(
+          setAuthorisation(
+            roles = listOf("ROLE_NON_ASSOCIATIONS"),
+            scopes = listOf("write"),
+          ),
+        )
+        .header("Content-Type", "application/json")
+        .exchange()
+        .expectStatus()
+        .isBadRequest
+
+      // unsupported Content-Type
+      webTestClient.post()
+        .uri(url)
+        .headers(
+          setAuthorisation(
+            roles = listOf("ROLE_NON_ASSOCIATIONS"),
+            scopes = listOf("write"),
+          ),
+        )
+        .header("Content-Type", "text/plain")
+        .bodyValue("{}")
+        .exchange()
+        .expectStatus()
+        .isBadRequest
+    }
+
+    @Test
+    fun `for a valid request creates the non-association`() {
+      throw Exception("TODO: Make valid request")
+
+      val expectedResponse = jsonString(mapOf("prisonerNumber" to prisonerNumber))
+      webTestClient.post()
+        .uri(url)
+        .headers(
+          setAuthorisation(
+            roles = listOf("ROLE_NON_ASSOCIATIONS"),
+            scopes = listOf("write", "read"),
+          ),
+        )
+        .exchange()
+        .expectStatus().isCreated
+        .expectBody().json(
+          expectedResponse,
+          true,
+        )
+
+      // TODO: Make request to `GET /non-associations/{id}` once it exists
+    }
+  }
+
+  @Nested
   inner class `GET non associations for a prisoner` {
 
     private val url = "/prisoner/$prisonerNumber/non-associations"
