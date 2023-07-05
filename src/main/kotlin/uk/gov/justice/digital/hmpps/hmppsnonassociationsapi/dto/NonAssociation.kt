b84901acf78f88b5293218cbd06b424b3ec5ca47
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2023-07-05 14:57:34 +0100

    NON-86: DTO classes for create request and single non-association response

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/dto/NonAssociation.kt
@@ -0,0 +1,57 @@
+package uk.gov.justice.digital.hmpps.hmppsnonassociationsapi.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+/**
+ * Non-association between two prisoners
+ */
+data class NonAssociation(
+  @Schema(description = "ID of the non-association", required = false, example = "42")
+  val id: Long,
+
+  @Schema(description = "Prisoner number to not associate", required = true, example = "A1234BC")
+  val firstPrisonerNumber: String,
+  @Schema(description = "Reason why this prisoner should be kept apart from the other", required = true, example = "VICTIM")
+  val firstPrisonerReason: NonAssociationReason,
+  @Schema(description = "Prisoner number to not associate", required = true, example = "D5678EF")
+  val secondPrisonerNumber: String,
+  @Schema(description = "Reason why this prisoner should be kept apart from the other", required = true, example = "PERPETRATOR")
+  val secondPrisonerReason: NonAssociationReason,
+
+  @Schema(description = "Type of restriction, e.g. don't locate in the same cell", required = true, example = "CELL")
+  val restrictionType: NonAssociationRestrictionType,
+
+  @Schema(description = "Type of restriction, e.g. don't locate in the same cell", required = true, example = "John and Luke always end up fighting")
+  val comment: String,
+  @Schema(description = "User ID of the person who created the non-association. NOTE: For records migrated from NOMIS/Prison API this is free text and may not be a valid User ID", required = true, example = "OFF3_GEN")
+  val authorisedBy: String,
+
+  @Schema(description = "Whether the non-association is closed or is in effect", required = true, example = "false")
+  val isClosed: Boolean = false,
+  @Schema(description = "User ID of the person who closed the non-association. Only present when the non-association is closed, null for open non-associations", required = false, example = "null")
+  val closedBy: String? = null,
+  @Schema(description = "Reason why the non-association was closed. Only present when the non-association is closed, null for open non-associations", required = false, example = "null")
+  val closedReason: String? = null,
+  @Schema(description = "Date and time of when the non-association was closed. Only present when the non-association is closed, null for open non-associations", required = false, example = "null")
+  val closedAt: String? = null,
+)
+
+/**
+ * Request format for creating a new, open, non-association between two prisoners
+ */
+data class CreateNonAssociationRequest(
+  @Schema(description = "Prisoner number to not associate", required = true, example = "A1234BC")
+  val firstPrisonerNumber: String,
+  @Schema(description = "Reason why this prisoner should be kept apart from the other", required = true, example = "VICTIM")
+  val firstPrisonerReason: NonAssociationReason,
+  @Schema(description = "Prisoner number to not associate", required = true, example = "D5678EF")
+  val secondPrisonerNumber: String,
+  @Schema(description = "Reason why this prisoner should be kept apart from the other", required = true, example = "PERPETRATOR")
+  val secondPrisonerReason: NonAssociationReason,
+
+  @Schema(description = "Type of restriction, e.g. don't locate in the same cell", required = true, example = "CELL")
+  val restrictionType: NonAssociationRestrictionType,
+
+  @Schema(description = "Type of restriction, e.g. don't locate in the same cell", required = true, example = "John and Luke always end up fighting")
+  val comment: String,
+)
