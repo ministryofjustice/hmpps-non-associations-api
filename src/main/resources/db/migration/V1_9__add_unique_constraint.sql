a6a18b8edb794b8e6659153c2d06131e08708448
Michael Willis <mike.willis@digital.cabinet-office.gov.uk>
2023-07-17 16:45:50 +0100

    NON-90 Remove wrong comment

--- src/main/resources/db/migration/V1_9__add_unique_constraint.sql
+++ src/main/resources/db/migration/V1_9__add_unique_constraint.sql
@@ -1,3 +1,5 @@
--- Increase length of `authorised_by` column to 60 chars as in NOMIS
+-- Add unique constraint to two prisoner numbers
 ALTER TABLE non_association
-ADD CONSTRAINT prisoner_association_unq UNIQUE (first_prisoner_number, second_prisoner_number)
+    ADD CONSTRAINT prisoner_association_unq UNIQUE (first_prisoner_number, second_prisoner_number);
+
+CREATE UNIQUE INDEX prisoner_association_unq_idx ON non_association (first_prisoner_number, second_prisoner_number);
