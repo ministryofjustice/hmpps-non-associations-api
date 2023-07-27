417f433efbb067e3282ab084c03b53f113c7077b
Igor <igor.ushkarev@digital.justice.gov.uk>
2023-07-27 12:23:43 +0100

    Set the clock to use the configured time zone rather than system (which might be incorrect)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/config/ClockConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/config/ClockConfiguration.kt
@@ -1,11 +1,15 @@
 package uk.gov.justice.digital.hmpps.hmppsnonassociationsapi.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.time.Clock
+import java.time.ZoneId
 
 @Configuration
-class ClockConfiguration {
+class ClockConfiguration(
+  @Value("\${spring.jackson.time-zone}") private val timeZone: String,
+) {
   @Bean
-  fun clock(): Clock? = Clock.systemDefaultZone()
+  fun clock(): Clock? = Clock.system(ZoneId.of(timeZone))
 }
