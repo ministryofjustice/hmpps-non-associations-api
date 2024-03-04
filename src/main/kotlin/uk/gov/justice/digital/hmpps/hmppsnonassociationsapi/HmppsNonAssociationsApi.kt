2f4ec1cc7560baeeb19a452bde69bbc1cb8450c9
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2024-03-04 15:19:02 +0000

    NON-347: WIP/Attempt to integrate reporting lib
    See similar PR: https://github.com/ministryofjustice/hmpps-prisoner-communication-monitoring-api/pull/504

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/HmppsNonAssociationsApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/HmppsNonAssociationsApi.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.hmppsnonassociationsapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 /**
  * Used as the system OAuth username when calling HMPPS APIs
@@ -9,6 +10,10 @@ import org.springframework.boot.runApplication
 const val SYSTEM_USERNAME = "NON_ASSOCIATIONS_API"
 
 @SpringBootApplication()
+@ComponentScan(
+  "uk.gov.justice.digital.hmpps.hmppsnonassociationsapi",
+  "uk.gov.justice.digital.hmpps.digitalprisonreportinglib",
+)
 class HmppsNonAssociationsApi
 
 fun main(args: Array<String>) {
