eae76de28f7d04cd64fb2bb038dda04c0b078f76
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-03-13 13:33:17 +0000

    Copy OpenAPI fixes from incentives

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/config/OpenApiConfiguration.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.info.License
 import io.swagger.v3.oas.models.media.DateTimeSchema
 import io.swagger.v3.oas.models.media.Schema
 import io.swagger.v3.oas.models.media.StringSchema
@@ -38,24 +39,23 @@ class OpenApiConfiguration(
       Info().title("Non-Associations API")
         .version(version)
         .description("API for viewing and managing non-associations for prisoners")
-        .contact(Contact().name("HMPPS Digital Studio").email("feedback@digital.justice.gov.uk")),
+        .contact(Contact().name("HMPPS Digital Studio").email("feedback@digital.justice.gov.uk"))
+        .license(License().name("MIT")),
     )
     .components(
-      Components().addSecuritySchemes(
-        "bearer-jwt",
-        SecurityScheme()
-          .type(SecurityScheme.Type.HTTP)
-          .scheme("bearer")
-          .bearerFormat("JWT")
-          .`in`(SecurityScheme.In.HEADER)
-          .name("Authorization"),
-      )
+      Components()
+        .addSecuritySchemes(
+          "bearer-jwt",
+          SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT"),
+        )
         .addSecuritySchemes(
           "hmpps-auth",
           SecurityScheme()
-            .flows(getFlows())
             .type(SecurityScheme.Type.OAUTH2)
-            .openIdConnectUrl("$oauthUrl/.well-known/openid-configuration"),
+            .flows(getFlows()),
         ),
     )
     .addSecurityItem(SecurityRequirement().addList("bearer-jwt", listOf("read", "write")))
