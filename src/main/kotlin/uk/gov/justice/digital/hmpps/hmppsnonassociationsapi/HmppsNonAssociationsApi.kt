ab99b4e898401736de968e42fe3272131bf80a38
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2024-03-20 16:37:28 +0000

    NON-347: Bump DPR library - remove unnecessary `@ComponentScan`

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/HmppsNonAssociationsApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/HmppsNonAssociationsApi.kt
@@ -2,7 +2,6 @@ package uk.gov.justice.digital.hmpps.hmppsnonassociationsapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 /**
  * Used as the system OAuth username when calling HMPPS APIs
@@ -10,10 +9,6 @@ import org.springframework.context.annotation.ComponentScan
 const val SYSTEM_USERNAME = "NON_ASSOCIATIONS_API"
 
 @SpringBootApplication()
-@ComponentScan(
-  "uk.gov.justice.digital.hmpps.hmppsnonassociationsapi",
-  "uk.gov.justice.digital.hmpps.digitalprisonreportinglib",
-)
 class HmppsNonAssociationsApi
 
 fun main(args: Array<String>) {
