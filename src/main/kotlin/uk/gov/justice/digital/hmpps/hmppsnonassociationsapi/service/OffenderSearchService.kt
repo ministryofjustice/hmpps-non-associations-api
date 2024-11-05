17e10c9479dae570423b9f8f77bc89206fb1359d
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-11-05 17:57:52 +0000

    Paginate calls to offender search to avoid 1000 prisoner number limit

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/service/OffenderSearchService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/service/OffenderSearchService.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppsnonassociationsapi.service
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
+import reactor.core.publisher.Flux
 import uk.gov.justice.digital.hmpps.hmppsnonassociationsapi.config.MissingPrisonersInSearchException
 import uk.gov.justice.digital.hmpps.hmppsnonassociationsapi.dto.offendersearch.OffenderSearchPrisoner
 
@@ -22,16 +23,23 @@ class OffenderSearchService(
       return emptyMap()
     }
 
-    val requestBody = mapOf("prisonerNumbers" to prisonerNumbers.toSet())
-
-    val foundPrisoners = offenderSearchWebClient
-      .post()
-      .uri("/prisoner-search/prisoner-numbers")
-      .header("Content-Type", "application/json")
-      .bodyValue(requestBody)
-      .retrieve()
-      .bodyToMono<List<OffenderSearchPrisoner>>()
+    val requests = prisonerNumbers
+      .toSet()
+      .chunked(900)
+      .map { pageOfPrisonerNumbers ->
+        val requestBody = mapOf("prisonerNumbers" to pageOfPrisonerNumbers)
+        offenderSearchWebClient
+          .post()
+          .uri("/prisoner-search/prisoner-numbers")
+          .header("Content-Type", "application/json")
+          .bodyValue(requestBody)
+          .retrieve()
+          .bodyToMono<List<OffenderSearchPrisoner>>()
+      }
+    val foundPrisoners = Flux.merge(requests)
+      .collectList()
       .block()!!
+      .flatten()
       .associateBy(OffenderSearchPrisoner::prisonerNumber)
 
     // Throw an exception if any of the prisoners searched were not found
