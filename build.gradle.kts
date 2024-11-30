d88b2a39c847343d91530e0921ed8628c5cca9b7
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-11-30 13:05:06 +0000

    Keep kotlin plugins < 2.1 since ktlint (and others??) seems to not work

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ import uk.gov.justice.digital.hmpps.gradle.RevealSecretsTask
 
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.0"
-  kotlin("plugin.jpa") version "2.1.0"
-  kotlin("plugin.spring") version "2.1.0"
+  kotlin("plugin.jpa") version "2.0.21"
+  kotlin("plugin.spring") version "2.0.21"
   idea
 }
 
