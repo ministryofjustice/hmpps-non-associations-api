a98b6804286148b1c1031cc86b13b9ec80b5c512
Aldo Giambelluca <xoen@users.noreply.github.com>
2025-08-11 16:05:35 +0100

    Use single principal as token cache key (#486)
    * Use single principal as token cache key
    
    Copied/adapted from: https://github.com/ministryofjustice/hmpps-manage-users-api/pull/319/files
    
    The `ClientCachingOAuth2AuthorizedClientService` has since been updated
    to work with newer versions of Spring Boot.
    
    * linting
    
    * Load OAuth2ClientConfiguration after ClientRegistrationRepository beans
    
    A lot of tests were failing because of no registrations being available
    even if they should, a bit of digging and it may be down to the order
    of initializations of the beans.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsnonassociationsapi/config/OAuth2ClientConfiguration.kt
@@ -0,0 +1,54 @@
+package uk.gov.justice.digital.hmpps.hmppsnonassociationsapi.config
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientPropertiesMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.registration.ClientRegistration
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
+import java.util.Optional
+
+@Configuration
+@ConditionalOnBean(ClientRegistrationRepository::class)
+class OAuth2ClientConfiguration {
+
+  /**
+   * Constructs a ClientRegistrationRepository with registrations from beans (Delius) and Spring configuration (Azure OIDC)
+   */
+  @Bean
+  fun clientRegistrationRepository(
+    properties: Optional<OAuth2ClientProperties>,
+    registrationBeans: Optional<List<ClientRegistration>>,
+  ): ClientRegistrationRepository? {
+    val registrations = ArrayList<ClientRegistration>()
+
+    if (properties.isPresent) {
+      registrations.addAll(OAuth2ClientPropertiesMapper(properties.get()).asClientRegistrations().values)
+    }
+
+    if (registrationBeans.isPresent) {
+      registrations.addAll(registrationBeans.get())
+    }
+
+    return InMemoryClientRegistrationRepository(registrations)
+  }
+
+  @Bean
+  fun authorizedClientManager(
+    clientRegistrationRepository: ClientRegistrationRepository,
+  ): OAuth2AuthorizedClientManager {
+    val authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build()
+    val authorizedClientManager =
+      AuthorizedClientServiceOAuth2AuthorizedClientManager(
+        clientRegistrationRepository,
+        ClientCachingOAuth2AuthorizedClientService(clientRegistrationRepository),
+      )
+    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
+    return authorizedClientManager
+  }
+}
